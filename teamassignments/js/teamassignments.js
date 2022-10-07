e91b25f3bf0f33b1336a41dbd140199cc79c396f
ikethompson88 <ikethompson88@byui.edu>
2022-10-06 20:58:18 -0700

    team activity

--- teamassignments/js/teamassignments.js
+++ teamassignments/js/teamassignments.js
@@ -3,6 +3,10 @@ const links = [
       label: "Week 3 Team Assignment",
       url: "week3/index-START.html"
     },
+    {
+      label: "Week 4 Team Assignment",
+      url: "week4/index.html"
+    },
   ]
 
 links.forEach(linkObject => {
