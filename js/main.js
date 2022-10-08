4cad0a87712a2203489d30a8808d34d99c402197
ikethompson88 <ikethompson88@byui.edu>
2022-10-07 20:53:44 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
-const links = [
-    {
-      label: "Team Assignments",
-      url: "teamassignments/index.html"
-    },
-    {
-      label: "Week 1 Reading Notes",
-      url: "week1/index.html"
-    },
-    {
-        label: "Week 2 Reading Notes",
-        url: "week2/index.html"
-    },
-    {
-      label: "Week 3 Reading Notes",
-      url: "week3/index.html"
+const links = [{
+    label: "Team Assignments",
+    url: "teamassignments/index.html"
+  },
+  {
+    label: "Week 1 Reading Notes",
+    url: "week1/index.html"
+  },
+  {
+    label: "Week 2 Reading Notes",
+    url: "week2/index.html"
+  },
+  {
+    label: "Week 3 Reading Notes",
+    url: "week3/index.html"
   },
   {
     label: "Week 4 Reading Notes",
     url: "week4/index.html"
-}
-  ]
+  }
+]
 
 links.forEach(linkObject => {
-    document.getElementById("toc").innerHTML += `<li><a href='${linkObject.url}'>${linkObject.label}</a></li>`
+  document.getElementById("toc").innerHTML += `<li><a href='${linkObject.url}'>${linkObject.label}</a></li>`
 })
